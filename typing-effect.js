05a8101f8f161135e9664bd68b7ee38a90ecf0ea
kgujral2612 <kgujral@pdx.edu>
2023-04-25 17:45:55 -0700

    Added typing effect and favicon

---
+++ typing-effect.js
@@ -0,0 +1,41 @@
+const typedTextSpan = document.querySelector("#typed-text");
+const typingCursorSpan = document.querySelector("#typing-cursor");
+
+const textArray = ["a Software Engineer", "an NLP Enthusiast", "a Creative Mind"];
+const typingDelay = 200;
+const erasingDelay = 100;
+const newTextDelay = 2000; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    if(!typingCursorSpan.classList.contains("typing")) typingCursorSpan.classList.add("typing");
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } 
+  else {
+    typingCursorSpan.classList.remove("typing");
+  	setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+	if (charIndex > 0) {
+    if(!typingCursorSpan.classList.contains("typing")) typingCursorSpan.classList.add("typing");
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } 
+  else {
+    typingCursorSpan.classList.remove("typing");
+    textArrayIndex++;
+    if(textArrayIndex>=textArray.length) textArrayIndex=0;
+    setTimeout(type, typingDelay + 1100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
+  if(textArray.length) setTimeout(type, newTextDelay + 250);
+});
